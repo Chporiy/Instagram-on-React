d132a06583799b223fb85d86e11b53987f352549
Chporiy <38475706+Chporiy@users.noreply.github.com>
2019-05-26 21:37:11 +0300

    2й день воркшопа Инстаграм на React.js. P.S.: Выполнил усложнённое задание

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,40 +1,73 @@
-import React from 'react';
+import React, {Component} from 'react';
 import User from './User';
+import InstaService from '../services/instaservice';
+import ErrorMessage from './ErrorMessage';
 
-export default function Users() {
-  return(
-    <div className="right">
-      <User 
-          src="https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg" 
-          alt="man"
-          name="user_man"/>
+
+export default class Users extends Component {
+  InstaService = new InstaService();
+
+  state = {
+    users: [],
+    error: false
+  }
+
+  componentDidMount() {
+    this.updateUsers();
+  }
+
+  updateUsers() {
+    this.InstaService.getAllPosts()
+    .then(this.onLoadUsers)
+    .catch(this.onError)
+  }
+
+  onLoadUsers = (posts) => {
+    this.setState({
+      users: posts,
+      error: false
+    });
+  }
+
+  onError = () => {
+    this.setState({
+      error: true
+    });
+  }
+
+  renderUsers(arr) {
+    return (
       <div className="users__block">
-        <User
-            src = "https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
-            alt = "man"
-            name = "user_man" 
-            min/>
-        <User
-            src = "https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
-            alt = "man"
-            name = "user_man" 
-            min/>
-        <User
-            src = "https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
-            alt = "man"
-            name = "user_man" 
-            min/>
-        <User
-            src = "https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
-            alt = "man"
-            name = "user_man" 
-            min/>
-        <User
-            src = "https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
-            alt = "man"
-            name = "user_man" 
-            min/>
+        {arr.map(item => {
+          const {name, altname, photo, id} = item;
+          if (id !== 1) {
+            return (
+              <User
+                  src = {photo}
+                  alt = {altname}
+                  name = {name}
+                  min/>
+            );
+          }
+        })}
       </div>
-    </div>
-  );
+    );
+  }
+  
+  render() {
+    const {error, users} = this.state;
+      if (error) {
+        return (<ErrorMessage/>);
+      }
+      const items = this.renderUsers(users);
+      return(
+        <div className="right">
+          <User
+                src = "https://ksassets.timeincuk.net/wp/uploads/sites/46/2017/03/Priti-Patel-re-sized.jpg"
+                alt = "Jessy"
+                name = "jessy_john" />
+          {items}
+        </div>
+    );
+  } 
 }
